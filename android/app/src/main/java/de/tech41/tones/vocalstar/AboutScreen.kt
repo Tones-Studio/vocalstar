533b2663e872c595d9bc8256c1c85b181f9cb8a9
tech41-de <mathias.dietrich@tech41.de>
2024-05-02 10:27:14 +0200

    android screens started

---
+++ android/app/src/main/java/de/tech41/tones/vocalstar/AboutScreen.kt
@@ -0,0 +1,14 @@
+package de.tech41.tones.vocalstar
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AboutScreen() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(text = "About")
+    }
+}
--- android/app/src/main/java/de/tech41/tones/vocalstar/MainActivity.kt
+++ android/app/src/main/java/de/tech41/tones/vocalstar/MainActivity.kt
@@ -4,18 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import de.tech41.tones.vocalstar.ui.theme.VocalstarTheme
 
-
-
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,10 +47,15 @@ class MainActivity : ComponentActivity() {
                  */
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    TabScreen()
+
+                    /*
                     Greeting(
                         name = "Android " + stringFromJNI(),
                         modifier = Modifier.padding(innerPadding)
                     )
+                     */
                 }
             }
         }
@@ -44,6 +66,42 @@ class MainActivity : ComponentActivity() {
     companion object {
         init {
             System.loadLibrary("vocalstar")
+        }
+    }
+}
+
+@Composable
+fun TabScreen() {
+    var tabIndex by remember { mutableIntStateOf(0) }
+
+    val tabs = listOf("Sing", "In-Out", "About")
+
+    Box(Modifier.safeDrawingPadding()) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            when (tabIndex) {
+                0 -> HomeScreen()
+                1 -> DeviceScreen()
+                2 -> AboutScreen()
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            TabRow(selectedTabIndex = tabIndex) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(text = { Text(title) },
+                        selected = tabIndex == index,
+                        onClick = { tabIndex = index },
+                        icon = {
+                            when (index) {
+                                0 -> Icon(imageVector = Icons.Default.Home, contentDescription = null)
+                                1 -> Icon(imageVector = Icons.Default.Info, contentDescription = null)
+                                2 -> Icon(
+                                    imageVector = Icons.Default.Settings,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                    )
+                }
+            }
         }
     }
 }
