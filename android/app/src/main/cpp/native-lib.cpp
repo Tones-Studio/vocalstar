091f7da6a923032e1e9bf884a832e1f925e3febb
tech41-de <mathias.dietrich@tech41.de>
2024-05-03 13:48:46 +0200

    adding C++ example

--- android/app/src/main/cpp/native-lib.cpp
+++ android/app/src/main/cpp/native-lib.cpp
@@ -1,26 +1,41 @@
 #include <jni.h>
 #include <string>
+#include <oboe/Oboe.h>
+#include "HelloOboeEngine.h"
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_de_tech41_tones_vocalstar_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
+extern "C" {
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_de_tech41_tones_vocalstar_Model_c_1updateVolume(JNIEnv *env, jobject thiz, jfloat vol) {
-  puts("update Volume");
-}
-extern "C"
-JNIEXPORT void JNICALL
-Java_de_tech41_tones_vocalstar_Model_c_1updateMute(JNIEnv *env, jobject thiz, jboolean b) {
-    puts("update Mute");
-}
-extern "C"
-JNIEXPORT void JNICALL
-Java_de_tech41_tones_vocalstar_Model_c_1updateSpeaker(JNIEnv *env, jobject thiz, jboolean b) {
-    puts("update is Speaker");
+//static HelloOboeEngine sEngine;
+
+
+    JNIEXPORT jstring JNICALL
+    Java_de_tech41_tones_vocalstar_MainActivity_stringFromJNI(
+            JNIEnv* env,
+            jobject /* this */) {
+        std::string hello = "Hello from C++";
+        return env->NewStringUTF(hello.c_str());
+    }
+
+    JNIEXPORT void JNICALL
+    Java_de_tech41_tones_vocalstar_Model_c_1updateVolume(JNIEnv *env, jobject thiz, jfloat vol) {
+        puts("update Volume");
+    }
+
+    JNIEXPORT void JNICALL
+    Java_de_tech41_tones_vocalstar_Model_c_1updateMute(JNIEnv *env, jobject thiz, jboolean b) {
+        puts("update Mute");
+    }
+
+    JNIEXPORT void JNICALL
+    Java_de_tech41_tones_vocalstar_Model_c_1updateSpeaker(JNIEnv *env, jobject thiz, jboolean b) {
+        puts("update is Speaker");
+    }
+
+
+    JNIEXPORT jstring JNICALL
+    Java_de_tech41_tones_vocalstar_AboutScreenKt_getVersions(JNIEnv *env, jclass clazz) {
+        oboe::Version version = oboe::Version();
+        return env->NewStringUTF(version.Text);
+    }
+
 }
--- android/app/src/main/java/de/tech41/tones/vocalstar/AboutScreen.kt
+++ android/app/src/main/java/de/tech41/tones/vocalstar/AboutScreen.kt
@@ -1,16 +1,23 @@
 package de.tech41.tones.vocalstar
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun AboutScreen() {
     Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = "About",  fontSize = 30.sp)
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = "Version: " + getVersions(),  fontSize = 17.sp)
     }
 }
+
+external fun getVersions(): String
